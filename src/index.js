48a62becd3719465c14a421d835cb0ee9582b1e3
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2019-09-14 19:19:37 +0530

    tested redux is working or not all fine

--- src/index.js
+++ src/index.js
@@ -3,6 +3,50 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+
+// let store = createStore(reducer);
+
+
+// ACTION
+
+const increment = () => {
+    return {
+        type: 'INCREMENT'
+    };
+};
+
+const decrement = () => {
+    return {
+        type: 'DECREMENT'
+    };
+};
+
+// REDUCER
+
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+    }
+
+};
+
+let store = createStore(counter);
+
+// Display
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+// DISPATCH
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(increment());
+store.dispatch({ type: 'INCREMENT' });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
