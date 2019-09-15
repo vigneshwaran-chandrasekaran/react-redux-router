03c7c56e7f7d83d9c78a48acb1b9ca1bc218058f
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2019-09-15 19:25:15 +0530

    for redux loging purpose installed redux-logger

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from "react-redux";
+import logger from 'redux-logger'
 import rootReducer from './reducers';
+import App from './App';
 
 // STORE
 const store = createStore(
     rootReducer,
-    compose(applyMiddleware(thunk),
+    compose(applyMiddleware(logger, thunk),
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 
